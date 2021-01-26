972decd2e889b4d387abf3fc61af8a97f7467503
yunnlo <49525938+yunnlo@users.noreply.github.com>
2021-01-26 20:39:11 +0800

    Update crazyrow.cpp

--- crazyrow.cpp
+++ crazyrow.cpp
@@ -4,26 +4,27 @@
 using namespace std;
 
 const int max_n =8;
-int n, ans;
+int n;
 int v1[max_n][max_n];
 void solve(){
     int a[max_n]={0};
     int sum =0;
     for(int i=0;i<n;i++){
-        int weight=2^8;
+        int weight=1;
         for(int q=0;q<n;q++){
-            weight *=0.5;
             a[i] += weight*v1[i][q];
+            weight *=2;
         }
     }
-    for (int q=0;q<n;q++){
-        for(int i=0; i<n;i++){
-        if(a[i]<a[i+1]){
+    for (int q=0;q<n-1;q++){
+        for(int i=0; i<n-1;i++){
+        if(a[i]>a[i+1]){
             swap(a[i],a[i+1]);
             sum++;
         }
     }
     }
+  
     printf("total step %d", sum);
 }
 
